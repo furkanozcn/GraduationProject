7d06e54d97d8fee342b7f3bd78a3416c3d392dbc
Furkan Özcan <fozcan2@hotmail.com>
2020-03-28 12:47:49 +0300

    Added some object

---
+++ Assets/Car/CarCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollision : MonoBehaviour
+{
+    public Car car;
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.tag == "Kenar")
+        {
+            Debug.Log("Araç temas");
+            Destroy(car);
+            car.changeCamera();
+        }
+    }
+}
